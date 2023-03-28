a9002be2018e02adf558ed5b0bd79b1a0a78cbfd
IrynaBoryna <borisov_ka@ukr.net>
2023-03-28 22:55:37 +0300

    11

--- src/index.js
+++ src/index.js
@@ -1,63 +1,64 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-// Notify.success("Hooray! We found totalHits images.");
-// Notify.info("We're sorry, but you've reached the end of search results.");
-import axios, {isCancel, AxiosError} from 'axios';
-// const axios = require('axios/dist/browser/axios.cjs');
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import NewCardGallery from './fetchImages';
 
 const newCardGallery = new NewCardGallery();
 let imagesArray = [];
+
+
 const refs = {
   form: document.querySelector('.search-form'),
-  buttonSubmit: document.querySelector('submit'),
+  buttonSubmit: document.querySelector('button'),
   imageCreateCard: document.querySelector(".gallery-item"),
   button: document.querySelector('.load-more'),
 }
 
+refs.button.style.display = "none";
 
 refs.form.addEventListener('submit', onSearch);
 refs.button.addEventListener('click', onLoadMore);
+refs.form.addEventListener('input', updateForm);
 
 function onSearch (e) {
  e.preventDefault();
  newCardGallery.inputSearch = e.currentTarget.elements.searchQuery.value;
- newCardGallery.resetPageNumber();
- refs.imageCreateCard.innerHTML= "";
- refs.button.style.display = "none";
- newCardGallery.fetchImages().then(createImagesList);
-
+ newCardGallery.fetchImages().then(imagesArray => {
+  
+  // const markup =  imageCard(imagesArray);
+  // refs.imageCreateCard.insertAdjacentHTML("beforeend", markup);
+  refs.imageCreateCard.insertAdjacentHTML("beforeend", imageCard(imagesArray));
+  gallery.refresh();
+});
+ refs.button.style.display = "block";
+ refs.buttonSubmit.style.display = "none";
 }
-
-
+     
 function onLoadMore() {
-  newCardGallery.fetchImages().then(createImagesList);
-  // refs.button.style.display = "none";
+  newCardGallery.fetchImages().then(imagesArray=> {
+    refs.imageCreateCard.insertAdjacentHTML("beforeend", imageCard(imagesArray));
+    gallery.refresh();
+  });
+  // newCardGallery.fetchImages().then(createImagesList(imagesArray));
 }
 
 
-// async function fetchImages(inputSearch) {
-//   try {
-// //     const response = await axios.get(`${url}?key={API}&q=${inputSearch}}${queryParams}`);
-//     console.log(response);
-//    } catch (error) {
-//     console.error(error);
-//   }
-// }
 
 function createImagesList() {
-   refs.imageCreateCard.insertAdjacentHTML("beforeend", imageCard());
-   refs.button.style.display = "";
-   refs.buttonSubmit.style.display = "none";
+   refs.imageCreateCard.insertAdjacentHTML("beforeend", imageCard(imagesArray));
+   gallery.refresh();
 }
 
    function imageCard (imagesArray) {
     return imagesArray
-    .map(({webformatURL, tags, likes, views, comments, downloads }) => {
+    .map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
      return `
      <li style = "list-style: none">
        <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width = 140px/>
+       <a class="gallery__item" href='${largeImageURL}'>
+       <img src="${webformatURL}" alt="${tags}" loading="lazy" width = 140px/>
+     </a>
   <div class="info">
     <p class="info-item" style = "font-size: 10px">
       <b>Likes: <span style = "font-style: italic">${likes}</span></b>
@@ -69,7 +70,7 @@ function createImagesList() {
       <b>Comments:<span style = "font-style: italic"> ${comments}</span></b>
     </p>
     <p class="info-item" style = "font-size: 10px">
-      <b>Downloads:<span style = "font-style: italic">${downloads}</span></b>
+      <b>Downloads:<span style = "font-style: italic"> ${downloads}</span></b>
     </p>
   </div>
 </div>
@@ -77,12 +78,17 @@ function createImagesList() {
  `;
 }).join('');
 }
-// //   largeImageURL - посилання на велике зображення.
+
  refs.imageCreateCard.style.display = "flex"; 
  refs.imageCreateCard.style.flexWrap = "wrap";
  refs.imageCreateCard.style.gap = "20px"; 
-//  if(inputSearch === "") {
-//   refs.countryList.innerHTML = "";
-//   refs.countryInfoCard.innerHTML= "";
-//   return
-//  }
+
+
+ const gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+ function updateForm(){
+  refs.buttonSubmit.style.display = "block";
+  refs.button.style.display = "none";
+  newCardGallery.resetPageNumber();
+ refs.imageCreateCard.innerHTML= "";
+ }
